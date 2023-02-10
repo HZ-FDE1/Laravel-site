ef61d80b52ac9de1b3ab680c9fc8f0874e991d12
UBaron <baro0002@hz.nl>
2023-02-10 20:40:18 +0100

    routes and everything works for 1.4

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,18 +4,17 @@
     <head>
         <meta charset="UTF8">
         <link rel="stylesheet" href="css/style-general.css">
-        <link rel="stylesheet" href="css/style-index.css">
-        <title>Home Page</title>
+        <link rel="stylesheet" href="css/style-profile.css">
+        <title>Profile</title>
     </head>
-
 
     <body>
     <div class="sidebar">
-        <a href="welcome" class="active">Home Page</a>
-        <a href="profile">Profile</a>
+        <a href="welcome">Home Page</a>
+        <a href="profile" class="active">Profile</a>
         <a href="dashboard">Dashboard</a>
         <a href="faq">FAQ</a>
-        <a href="blog" >Blog</a>
+        <a href="blog">Blog</a>
         <a>&nbsp</a>
         <a href="https://teams.microsoft.com/l/channel/19%3a2e2afa0286b04932be16cb8ad2d9d2c0%40thread.skype/Algemeen?groupId=95bddebc-a340-4d88-81fc-b80e0bfc70c3&amp;tenantId=4c16deb3-342d-4fca-bcd5-b1429308034c">Teams</a>
         <a>------------------</a>
@@ -40,47 +39,43 @@
         <a class="Githublink" href="https://github.com/UBaron">Ontdek mijn werk!</a>
     </header>
 
-    <h1 class="typewriter"> Hi, ik ben Uriël :)</h1>
     <main>
-        <div class="hoverpic">
-            <img class="profilepicback" src="img/unicornme.png" alt="ProfilePic2">
-            <img class="profilepicfront" src="img/WeLAN.png" alt="ProfilePic1">
+        <h1 class="profiletitle">Wie ben ik?</h1>
 
 
-            <section class="waaromict">
-                <h1> <u>Waarom past ICT bij mij?</u></h1>
+        <img class="planeimg"  src="/img/plane.jpg" alt="planepic" width="300px" height="300px">
+        <img class="zeelandimg" src="/img/zeeland.png" alt="Zeeland" width="150px" height="150px">
 
-                <h2>
-                    <p> De reden waarom ik denk dat ICT bij mij past,
-                        is dat ik ben opgegroeid met (spel)computers.
-                        Zo vind ik het interessant hoe een computer, website of game werkt.
-                        Omdat ik het atlijd zo interessant heb gevonden,
-                        denk ik deze opleiding goed bij mij past.
-                    </p>
-                    <p>
-
-                    </p>
-                    <p> Na mijn opleiding wil ik mezelf specialiseren in het vakgebied
-                        <u>Cyber Security</u>. Als Ethisch Hacker moet je de kwetsbaarheden vinden
-                        in soft- en hardware, en daarvoor moet je goed out of the box kunnen denken.
-                        En dat is ook de reden waarom het mij zo interessant vind, zo moet je
-                        proberen een weg te vinden om in die bepaalde soft- of hardware te komen.
-                    </p>
-                </h2>
-            </section>
-
-            <nav>
-                <a href="profile.html"> Profile </a>
-                <a href="dashboard.html">Dashboard</a>
-                <a href="faq.html">FAQ</a>
-                <a href="blog.html">Blog</a>
-            </nav>
-
+        <section>
+            <article class="textprofile">
+                <h2> Persoonlijke Gegevens:</h2>
+                <ul>
+                    <li>Uriël Baron</li>
+                    <li>18 jaar</li>
+                    <li>Ik woon in Tholen, Zeeland</li>
+                    <li>Vooropleiding: Havo E+M</li>
+                </ul>
+                <h2> Mijn hobbies:</h2>
+                <ul>
+                    <li>Werk graag → Vastgoedbeveiliger</li>
+                    <li>Gamen → Rainbow Six Siege</li>
+                    <li>Rugbyen</li>
+                </ul>
+                <h2> Mijn Toekomst</h2>
+                <ol>
+                    <li> Bachelor → Specialiseren in het vakgebied Cyber Security </li>
+                    <li> Defensie/ bedrijf</li>
+                    <li> Eigen bedrijf/ uitgekocht worden door een groot bedrijf</li>
+                </ol>
+            </article>
+        </section>
+        <nav>
+            <a href="./index.html"> Index </a>
+        </nav>
     </main>
-
     </body>
-
     </html>
+
 
     <Directhost "C:/Users/baron/Desktop/GIT PULL"
     <Directory "C:/Users/baron/Desktop/GIT PULL"
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FaqController;
+use App\Http\Controllers\BlogController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,21 +17,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/posts/{post}',  function($post){
-    $posts = [
-        'my-first-post' => 'Hello, this is my first blog post!',
-        'my-second-post' => 'Now I am getting the hang of this blogging thing.'
-    ];
+//Route::get('/posts/{post}',  function($post){
+//    $posts = [
+//        'my-first-post' => 'Hello, this is my first blog post!',
+//        'my-second-post' => 'Now I am getting the hang of this blogging thing.'
+//    ];
+//
+//    if (!array_key_exists($post, $posts)) {
+//        abort(404, 'Sorry, that post was not found.');
+//    }
 
-    if (!array_key_exists($post, $posts)) {
-        abort(404, 'Sorry, that post was not found.');
-    }
+//    return view('post', [
+//        'post' => $posts[$post]
+//    ]);
+//});
 
-    return view('post', [
-        'post' => $posts[$post]
-    ]);
-});
+Route::get('/welcome', [WelcomeController::class, 'show']);
+Route::get('/profile', [ProfileController::class, 'show']);
+Route::get('/dashboard',[DashboardController::class, 'show']);
+Route::get('/faq',[FaqController::class, 'show']);
+Route::get('/blog',[BlogController::class, 'show']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
