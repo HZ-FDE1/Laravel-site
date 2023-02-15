0f7f22df22f755ab7b04d6be9e581c92eeb98d6e
UBaron <baro0002@hz.nl>
2023-02-15 23:27:19 +0100

    grade table added and all the things that make it work

--- database/migrations/2023_02_14_145958_create_grades_table.php
+++ database/migrations/2023_02_14_145958_create_grades_table.php
@@ -14,12 +14,19 @@ return new class extends Migration
     public function up()
     {
         Schema::create('grades', function (Blueprint $table) {
-            $table->id('bigIncrements');
+            $table->id();
             $table->string('course_name');
             $table->string('test_name');
-            $table->decimal('lowest_passing_grade');
-            $table->decimal('best_grade');
-            $table->date('passed_at');
+
+            $table->decimal('lowest_passing_grade', $precision = 3, $scale = 1)
+                ->default(5.5)->comment('Lowest grade to pass so average calculations can be applied.');
+
+            $table->decimal('best_grade', $precision = 3, $scale = 1)->nullable();
+
+            $table->date('passed_at')->nullable()
+                ->comment('Date that the grade was larger than or equal to the lowest passing grade.');
+
+            $table->timestamps();
         });
     }
 
